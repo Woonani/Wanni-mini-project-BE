d3152f12adc007f80b1b8306be9571127fcbd80e
nkh91 <khnam5@naver.com>
2022-10-11 19:04:50 +0900

    join done

---
+++ routes/join.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// const passport = require('passport');
+const bcrypt = require('bcrypt');
+// const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  const { email, name , password, nick, className, phoneNum   } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      res.status(403).send('이미 존재하는 email 입니다.');
+    }else{
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+          email,
+          name ,
+          password: hash,
+          nick,
+          className,
+          phoneNum
+        });
+        res.status(201).json({
+            code: 201,
+            message: "success"
+        })
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+module.exports = router;
